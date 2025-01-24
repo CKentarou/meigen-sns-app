8faf7173bf28cefa027ae086d43ba53bd6094ffb
CLIFF <k023c0018@g.neec.ac.jp>
2025-01-24 23:17:55 +0000

    開いているページにリダイレクトする

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,14 @@ class FavoritesController < ApplicationController
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: post.id)
     favorite.save
-    redirect_to posts_path
+    redirect_to request.referer
   end
 
   def destroy
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.find_by(post_id: post.id)
     favorite.destroy
-    redirect_to posts_path
+    #リダイレクトではなくページをリロードする使用に今後する
+    redirect_to request.referer
   end
 end
