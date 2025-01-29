60c33b14b6673f06ed7d602de6e752aab3c4f895
CLIFF <k023c0018@g.neec.ac.jp>
2025-01-29 11:30:27 +0000

    リダイレクト先のページをアクションが実行されたページに変更

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,7 +3,7 @@ class FavoritesController < ApplicationController
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: post.id)
     favorite.save
-    redirect_to posts_path(sort_by: "session")
+    redirect_to request.referer
   end
 
   def destroy
@@ -11,6 +11,6 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.find_by(post_id: post.id)
     favorite.destroy
     #リダイレクトではなくページをリロードする使用に今後する
-    redirect_to posts_path(sort_by: "session")
+    redirect_to request.referer
   end
 end
