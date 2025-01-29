a2e5eaf1972814cf29870465508c838134294e9d
CLIFF <k023c0018@g.neec.ac.jp>
2025-01-29 11:36:11 +0000

    sort_by: sessionを適用

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,7 +3,10 @@ class FavoritesController < ApplicationController
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: post.id)
     favorite.save
-    redirect_to request.referer
+    redirect_to URI.parse(request.referer).tap { |uri|
+      query_params = Rack::Utils.parse_query(uri.query).merge("sort_by" => "session")
+      uri.query = query_params.to_query
+    }.to_s
   end
 
   def destroy
@@ -11,6 +14,9 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.find_by(post_id: post.id)
     favorite.destroy
     #リダイレクトではなくページをリロードする使用に今後する
-    redirect_to request.referer
+    redirect_to URI.parse(request.referer).tap { |uri|
+      query_params = Rack::Utils.parse_query(uri.query).merge("sort_by" => "session")
+      uri.query = query_params.to_query
+    }.to_s
   end
 end
